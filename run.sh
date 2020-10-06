1d34e57e095e77c4ea9cdd74b06993b8c0cb7ba6
Jeon MacPro <mc3767@gmail.com>
2020-10-06 10:58:23 +0900

    postgre10.yum

--- run.sh
+++ run.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-bundle exec jekyll serve
+bundle exec jekyll serve --trace --watch
+
+# 필요설치 ( 글로벌 설치 - https://jekyllrb-ko.github.io/docs/installation/macos/ )
+
+# Mojave 버전 (10.14)Permalink
+# Mojave 의 시스템 무결성 보호 (SIP) 기능 때문에, 다음과 같이 실행해야 합니다:
+
+# sudo gem install bundler
+# sudo gem install -n /usr/local/bin/ jekyll
+# sudo gem install -n /usr/local/bin/ jekyll-feed
+
+# error
+# --> Could not find multipart-post-2.1.1 in any of the sources
+#     Run `bundle install` to install missing gems.
+# ==> bundle update --bundler
